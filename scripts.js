7faf45dee22e4f316194b3345bc099855c21912c
Maria Luiza M <maria.lmm.975@gmail.com>
2022-02-16 11:43:44 -0300

    Adiciona funções da primeira tela de criação

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+//variáveis
+let quizzDoUsuario = null;
+
 //obterQuizzes();
 
 function obterQuizzes(){
@@ -22,4 +25,38 @@ function quizzesObtidos(quizzes){
 function renderizaCapaDoQuizz(quizz){
 
    
+}
+
+
+//criação do quizz
+
+function criarQuizz(){
+    const criacao = document.querySelector('.criacao');
+    criacao.classList.remove('escondido');
+}
+
+function coletarInformacoesBasicas(){
+    const tituloQuizz = document.querySelector('.titulo-quizz').value;
+    const imagemQuizz = document.querySelector('.imagem-quizz').value;
+    const quantidadePerguntas = document.querySelector('.quantidade-perguntas').value;
+    const quantidadeNiveis = document.querySelector('.quantidade-niveis').value;
+    if (validarInformacoesBasicas(tituloQuizz, imagemQuizz, quantidadePerguntas, quantidadeNiveis)){
+        coletarPerguntas(quantidadePerguntas);
+    } else {
+        alert('Preencha as informações corretamente!');
+    }
+}
+
+function validarInformacoesBasicas(titulo, imagem, perguntas, niveis){
+    const verificacaoTitulo = titulo.length >= 20 && titulo.length <= 65;
+    const verificacaoImagem = imagem.slice(0, 4) === 'http' && imagem.slice(5, 8) === '://';
+    const verificacaoPerguntas = parseInt(perguntas) >= 3;
+    const verificacaoNiveis = parseInt(niveis) >= 2;
+    const verificacaoFinal = verificacaoTitulo && verificacaoImagem && verificacaoPerguntas && verificacaoNiveis;
+    return verificacaoFinal
+}
+
+function coletarPerguntas(quantidade){
+    const comeco = document.querySelector('.comeco');
+    comeco.classList.add('escondido');
 }
